1e22262aca80ea274d68680eda1fc710c6785886
Szczepan <szczepan.staszak@gmail.com>
2024-02-10 16:44:50 +0100

    Use real key

--- components/wmbus/wmbus.cpp
+++ components/wmbus/wmbus.cpp
@@ -102,7 +102,7 @@ namespace wmbus {
           sprintf(hexString,"%08X", meter_id);
           //
           char hexKey[32+1];
-          int i=0
+          int i=0;
           for (auto it = sensor->key.begin(); it != sensor->key.end(); it++) {
             sprintf(hexKey[i],"%02X", *it);
             i++;
