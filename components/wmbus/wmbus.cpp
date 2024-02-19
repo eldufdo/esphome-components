7db00260b6161200b3a958e22ca4021f53cbac03
eldufdo <eldufdo@users.noreply.github.com>
2024-02-19 22:52:09 +0100

    Update wmbus.cpp

--- components/wmbus/wmbus.cpp
+++ components/wmbus/wmbus.cpp
@@ -131,8 +131,6 @@ namespace wmbus {
             double total_last_month = meter->getNumericValue("last_month_total", Unit::M3);
             ESP_LOGI(TAG, "Total Last month: %.4f", total_last_month);
             this->wmbus_listeners_[meter_id]->sensors_["last_month_total_water_m3"]->publish_state(total_last_month);
-            std::string current_alarm = meter->getStringValue(meter->findFieldInfo("current_alarms", Quantity::Unknown));
-            ESP_LOGI(TAG, "alarm %s", current_alarm);
             auto mapValues = selected_driver->get_values(frame);
             if (mapValues.has_value()) {
               if (this->wmbus_listeners_[meter_id]->sensors_.count("lqi") > 0) {
