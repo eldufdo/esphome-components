453661103b288486068221cf0fa26bfd3296104e
eldufdo <eldufdo@users.noreply.github.com>
2024-02-20 21:58:43 +0100

    Update wmbus.cpp

--- components/wmbus/wmbus.cpp
+++ components/wmbus/wmbus.cpp
@@ -128,7 +128,7 @@ namespace wmbus {
             double total = meter->getNumericValue("total", Unit::M3);
             ESP_LOGI(TAG, "Total: %.4f", total);
             this->wmbus_listeners_[meter_id]->sensors_["total_water_m3"]->publish_state(total);
-            /*double total_last_month = meter->getNumericValue("last_month_total", Unit::M3);
+            double total_last_month = meter->getNumericValue("last_month_total", Unit::M3);
             ESP_LOGI(TAG, "Total Last month: %.4f", total_last_month);
             this->wmbus_listeners_[meter_id]->sensors_["last_month_total_water_m3"]->publish_state(total_last_month);
             auto mapValues = selected_driver->get_values(frame);
